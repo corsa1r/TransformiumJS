00202a8da59d18151eec1b13285db02c798e8690
Vladimir Enchev <vladimir.corsair@gmail.com>
2014-12-29 17:10:11 +0200

    Update Camera.js
    Bug fix

--- src/engine/render/Camera.js
+++ src/engine/render/Camera.js
@@ -1,6 +1,8 @@
 /**
  * @file implements Camera Class
  * @author CORSAIR <vladimir.corsair@gmail.com>
+ * 
+ * @version 1
  * */
 ;(function() {
     
@@ -16,9 +18,14 @@
             this.position = Camera.$$defaults.position.clone();
         });
         
+        /**
+         * This method returns you boolean true, if the given gameObject is on visible part of the screen.
+         * @param {GameObject} gameObject - reference to gameObject which you want to know if is visible.
+         * @returns Boolean
+         */
         Camera.prototype.sees = function(gameObject) {
             var c1 = this.$$screen.innerPoint(gameObject.position);
-            var c2 = this.$$screen.innerPoint(gameObject.size);
+            var c2 = this.$$screen.innerPoint(gameObject.size.append(gameObject.position));
             return Boolean(c1 || c2);
         };
         
